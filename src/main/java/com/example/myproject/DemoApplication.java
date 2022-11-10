4433224d3b5f664784680fc00bc3e2ccb80d972b
root <root@ip-172-31-90-79.ec2.internal>
2022-11-10 12:16:07 +0000

    Added /CheckSession endpoint

--- src/main/java/com/example/myproject/DemoApplication.java
+++ src/main/java/com/example/myproject/DemoApplication.java
@@ -3,6 +3,11 @@ package com.example.myproject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.HttpSession;
+
 @SpringBootApplication
 public class DemoApplication {
 
@@ -10,4 +15,14 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+}
+
+@RestController
+class MyController{
+
+	@PostMapping("/CheckSession")
+	public String checkSession(HttpSession session){
+		boolean hasSession = session.getAttribute("userId") != null;
+		return "{ \"error\": false, \"hasSession\": " + String.valueOf(hasSession) + " }";
+	}
 }
